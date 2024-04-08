f1cacd3ba7d3f3bb8e46f94446988a342c2c8e1b
hwtzzz <2975612823@qq.com>
2024-04-08 17:29:58 +0800

    初始化前端登录注册页面

---
+++ TTHappyAPP/pages/register/register.js
@@ -0,0 +1,43 @@
+const app = getApp()
+const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+
+
+Page({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        avatarUrl: defaultAvatarUrl,
+        theme: wx.getSystemInfoSync().theme,
+    },
+
+    onChooseAvatar(e) {
+        const {
+            avatarUrl
+        } = e.detail
+
+        this.setData({
+            avatarUrl,
+        })
+        // app.globalData.userInfo.avatarUrl = avatarUrl
+
+    },
+    formSubmit(e) {
+        app.globalData.userInfo.nickName = e.detail.value.nickname
+        wx.switchTab({
+            url: '/pages/index/index',
+        })
+    },
+
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+        wx.onThemeChange((result) => {
+            this.setData({
+                theme: result.theme
+            })
+        })
+    }
+})
